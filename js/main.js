605172403121d686322236f8ed95ad38a4ec5542
Loic Dupong <loic@dupong.be>
2025-05-16 14:10:09 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ const inputPrice = document.getElementById('prix');
 const inputQuantity = document.getElementById('quantite');
 const messageHTML = document.querySelector('.message');
 const totalHTML = document.querySelector('.total--price');
+const commandeHTML = document.querySelector('.commande');
+
 const btnGenerate = document.querySelector('.btn');
+const btnDelete = document.querySelector('.btn--delete');
 
 const commandesTab = [];
 let total = Number.parseFloat(0).toFixed(2);
@@ -29,10 +32,16 @@ function displayTab() {
     commandesTab.forEach(commande => {
         let div = document.createElement('div');
         div.className = `${commande.name} commande`;
-        div.innerHTML += `${commande.returnCommande()}`
+        div.setAttribute("data-index", `${commandesTab.indexOf(commande)}`);
+        div.innerHTML += `${commande.returnCommande()} <i class="fa-solid fa-square-minus btn--delete"></i>`
         messageHTML.append(div);
     })
-};
+    if (commandesTab.length === 0) {
+    messageHTML.innerHTML = "<p>⛔ Aucune commande ajoutée.</p>";
+}
+}
+
+displayTab();
 
 // === Reset les input ===
 function resetInput() {
@@ -50,11 +59,31 @@ function totalPrice() {
 }
 
 
+
+
 // === Event Listner btn ===
+// == Add
 btnGenerate.addEventListener('click', (e) => {
-    e.preventDefault();
-    commandesTab.push(new Commande(inputName.value,inputQuantity.value, inputPrice.value));
-    displayTab();
-    totalPrice();
-    resetInput();
+    if (inputName.value && !isNaN(inputPrice.value) && !isNaN(inputQuantity.value)) {
+        e.preventDefault();
+        commandesTab.push(new Commande(inputName.value,inputQuantity.value, inputPrice.value));
+        displayTab();
+        totalPrice();
+        resetInput();
+    } else {
+        alert('Veuillez remplir les champs du formulaire.');
+    }
+    
+    
+})
+
+// == Delete
+messageHTML.addEventListener('click', (e) =>{
+    if (e.target.classList.contains('btn--delete')) {
+      let index = Number(e.target.parentElement.dataset.index);
+      commandesTab.splice(index, 1);
+      
+      displayTab();
+      totalPrice();
+    }
 })
